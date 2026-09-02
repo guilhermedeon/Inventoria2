522c07bed8ea563f2a5ffd3ab41833017b687c37
Guilherme <guibyn@gmail.com>
2026-09-01 21:36:58 -0300

    chore: add entity generic domain events

---
+++ src/Domain/Common/DomainEvents/EntityBaseDomainEvents.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedKernel;
+
+namespace Domain.Common.DomainEvents;
+
+public record EntityCreatedDomainEvent<T>(Guid Id) : IDomainEvent where T : Entity;
+
+public record EntityDeletedDomainEvent<T>(Guid Id) : IDomainEvent where T : Entity;
+
+public record EntityUpdatedDomainEvent<T>(Guid Id) : IDomainEvent where T : Entity;
