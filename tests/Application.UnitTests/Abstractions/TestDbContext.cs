9c2e4c1750d167ba4b0ea86fcc61f2d532c5edea
Guilherme Deon de Faria <guibyn@gmail.com>
2026-09-07 17:33:50 -0300

    fix: add entity configurations to test db context

--- tests/Application.UnitTests/Abstractions/TestDbContext.cs
+++ tests/Application.UnitTests/Abstractions/TestDbContext.cs
@@ -1,6 +1,10 @@
 using Application.Abstractions.Data;
+using Domain.Classifications;
+using Domain.Inventories;
+using Domain.Items;
 using Domain.Todos;
 using Domain.Users;
+using Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 
 namespace Application.UnitTests.Abstractions;
@@ -17,4 +21,17 @@ public sealed class TestDbContext(DbContextOptions<TestDbContext> options)
     public DbSet<RefreshToken> RefreshTokens { get; set; }
 
     public DbSet<TodoItem> TodoItems { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+    public DbSet<Inventory> Inventory { get; set; }
+    public DbSet<InventoryItem> InventoryItems { get; set; }
+    public DbSet<ItemDefinition> ItemDefinitions { get; set; }
+    public DbSet<ItemVariant> ItemVariants { get; set; }
+    public DbSet<PackDefinition> PackDefinitions { get; set; }
+    public DbSet<PackDefinitionItem> PackDefinitionItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
 }
