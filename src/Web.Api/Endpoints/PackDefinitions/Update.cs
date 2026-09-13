457ee0e3ebcb3c332ee419cd63002d385104e52c
Guilherme Deon de Faria <guibyn@gmail.com>
2026-09-13 09:11:03 -0300

    fix: build errors

--- src/Web.Api/Endpoints/PackDefinitions/Update.cs
+++ src/Web.Api/Endpoints/PackDefinitions/Update.cs
@@ -42,11 +42,7 @@ internal sealed class Update : IEndpoint
                 request.Sku,
                 request.Barcode,
                 request.CategoryId,
-                request.Items.Select(i => new PackItemDto
-                {
-                    ItemVariantId = i.ItemVariantId,
-                    Quantity = i.Quantity
-                }).ToList()
+                [.. request.Items.Select(i => new PackItemDto(i.ItemVariantId, i.Quantity))]
             );
 
             Result result = await handler.Handle(command, cancellationToken);
